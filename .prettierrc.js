da0d78d9185be08f564b3d194aef51b50997d339
Nycolas Santana <nycolassantana00@gmail.com>
2022-02-23 14:11:10 -0300

    initial config of project, eslint, prettier, vite

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: "auto",
+  prettier: [
+    'error',
+    {
+      'endOfLine': 'auto',
+    }
+  ]
+}
